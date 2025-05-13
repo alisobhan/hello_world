517eca2a71e9d9c281d34d3143fc56278dd74542
Ali Sobhani <acdota@outlook.com>
2025-05-13 19:35:35 +0330

    dgfsghdf

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use std::io::stdin;
+
 
 fn main() {
     println!("Guess the number please!");
     println!("Please input your guess.");
     let mut guess = String::new();
 
-    let generated = rand::rng();
 
-    // stdin().read_line(&mut guess).expect("Failed to read guess.");
+    stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read guess.");
 
     
-    println!({generated.random::<char>});
+    println!("You Guesses {guess}");
 }
 
